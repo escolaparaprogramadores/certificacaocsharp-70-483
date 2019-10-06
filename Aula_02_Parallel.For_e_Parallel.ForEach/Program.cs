0dba16e161cb35fa8a86c4fcf44694dfa14508d9
Mark <escolaparaprogramadores@gmail.com>
2019-10-06 17:52:07 -0300

    Aula_02_ParallelFor_e_ParallelForEach

---
+++ Aula_02_Parallel.For_e_Parallel.ForEach/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Aula_01_TaskParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Parallel.Invoke(()=> ImprimirZero(), ()=> ImprimirUm(), ()=> ImprimirPonto());
+
+            stopwatch.Stop();
+            System.Console.WriteLine( );
+            System.Console.WriteLine(" A tarefa foi finalizada {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+
+           
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            ImprimirZero();
+            ImprimirUm();
+            ImprimirPonto();
+
+            stopwatch.Stop();
+            System.Console.WriteLine(" A tarefa foi finalizada {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+        }
+
+        private static void ImprimirPonto()
+        {
+        //    for (int i = 0; i < 300; i++)
+        //    {
+        //        ExecutarTarefaDemorada();
+        //        System.Console.Write(".");
+        //    }
+
+        //    Parallel.For(0, 300, (i)=>
+        //    {
+        //        ExecutarTarefaDemorada();
+        //        System.Console.Write(".");
+        //    });
+
+           var lista = Enumerable.Range(0, 300);
+           Parallel.ForEach(lista, (item)=>
+           {
+               ExecutarTarefaDemorada();
+               System.Console.Write(".");
+           });
+        }
+        private static void ImprimirUm()
+        {
+        //    for (int i = 0; i < 300; i++)
+        //    {
+        //        ExecutarTarefaDemorada();
+        //        System.Console.Write("1");
+        //    }
+
+        //       Parallel.For(0, 300, (i)=>
+        //    {
+        //        ExecutarTarefaDemorada();
+        //        System.Console.Write("1");
+        //    });
+
+           var lista = Enumerable.Range(0, 300);
+           Parallel.ForEach(lista, (item)=>
+           {
+               ExecutarTarefaDemorada();
+               System.Console.Write("1");
+           });
+        }
+        private static void ImprimirZero()
+        {
+        //    for (int i = 0; i < 300; i++)
+        //    {
+        //        ExecutarTarefaDemorada();
+        //        System.Console.Write("0");
+        //    }
+
+        //       Parallel.For(0, 300, (i)=>
+        //    {
+        //        ExecutarTarefaDemorada();
+        //        System.Console.Write("0");
+        //    });
+
+           var lista = Enumerable.Range(0, 300);
+           Parallel.ForEach(lista, (item)=>
+           {
+               ExecutarTarefaDemorada();
+               System.Console.Write("0");
+           });
+        }
+        private static void ExecutarTarefaDemorada()
+        {
+            Thread.Sleep(10);
+        }
+    }
+}
