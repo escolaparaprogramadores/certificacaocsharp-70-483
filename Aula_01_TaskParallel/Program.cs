1735391517ad8beebc92cb1fe3dec78ba24ab025
Mark <escolaparaprogramadores@gmail.com>
2019-10-06 16:33:31 -0300

    Revert "Aula_01_Task_Parallel"
    This reverts commit 72dac7a9989cda642b17d12d4cf71cba07769cc5.

--- Aula_01_TaskParallel/Program.cs
+++ Aula_01_TaskParallel/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace Aula_01_TaskParallel
 {
@@ -8,57 +6,7 @@ namespace Aula_01_TaskParallel
     {
         static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            Parallel.Invoke(()=> ImprimirZero(), ()=> ImprimirUm(), ()=> ImprimirPonto());
-
-            stopwatch.Stop();
-            System.Console.WriteLine( );
-            System.Console.WriteLine(" A tarefa foi finalizada {0}", stopwatch.ElapsedMilliseconds / 1000.0);
-
-           
-            stopwatch.Reset();
-            stopwatch.Start();
-
-            ImprimirZero();
-            ImprimirUm();
-            ImprimirPonto();
-
-            stopwatch.Stop();
-            System.Console.WriteLine(" A tarefa foi finalizada {0}", stopwatch.ElapsedMilliseconds / 1000.0);
-        }
-
-        private static void ImprimirPonto()
-        {
-           //for (int i = 0; i < 300; i++)
-           Parallel.For(0, 300, (i)=> 
-           {
-               ExecutarTarefaDemorada();
-               System.Console.Write(".");
-           });
-        }
-        private static void ImprimirUm()
-        {
-            //for (int i = 0; i < 300; i++)
-           Parallel.For(0, 300, (i)=> 
-           {
-               ExecutarTarefaDemorada();
-               System.Console.Write("1");
-           });
-        }
-        private static void ImprimirZero()
-        {
-           //for (int i = 0; i < 300; i++)
-           Parallel.For(0, 300, (i)=> 
-           {
-               ExecutarTarefaDemorada();
-               System.Console.Write("0");
-           });
-        }
-        private static void ExecutarTarefaDemorada()
-        {
-            Thread.Sleep(10);
+            Console.WriteLine("Hello World!");
         }
     }
 }
