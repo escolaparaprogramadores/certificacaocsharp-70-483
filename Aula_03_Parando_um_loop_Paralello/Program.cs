102504fc3a63011f1b68dc2a4129ee29b3b44324
Mark <escolaparaprogramadores@gmail.com>
2019-10-13 00:34:15 -0300

    Aula_03_Parando_um_loop_Paralelo_Break()_Stop()

---
+++ Aula_03_Parando_um_loop_Paralello/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula_03_Parando_um_loop_Paralello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            // for (int i = 0; i < 300; i++)
+            // {
+            //     if(i == 99)
+            //     {
+            //         System.Console.WriteLine($"O loop foi parado na posição {i}");
+            //         break;
+            //     }
+            // }  
+
+            Parallel.For(0, 300, (int i, ParallelLoopState state)=>
+            {
+                if(i == 99)
+                {
+                    System.Console.WriteLine($"O loop foi parado na posição {i}");
+                    state.Stop();
+                }
+
+            });  
+
+            var lista = Enumerable.Range(0, 300);
+            Parallel.ForEach(lista, (int i, ParallelLoopState state)=>
+            {
+                if(i == 99)
+                {
+                    System.Console.WriteLine($"O loop foi parado na posição {i}");
+                    state.Break();
+                }
+
+            }); 
+
+
+
+
+        }
+    }
+}
+
